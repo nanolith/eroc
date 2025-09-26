ddba52b5c3c9d5c874331553d99e4c46bf5a3cda
Justin Handville <nanolith@gmail.com>
2025-09-26 02:20:50 -0400

    eroc_list_append impl.

---
+++ src/lib/eroc_list_append.c
@@ -0,0 +1,34 @@
+/**
+ * \file lib/eroc_list_append.c
+ *
+ * \brief Append a node at the end of a list.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <eroc/list.h>
+
+/**
+ * \brief Append a node at the end of a list.
+ *
+ * \param list          The list to use for appending.
+ * \param node          The node to append.
+ */
+void eroc_list_append(eroc_list* list, eroc_list_node* node)
+{
+    node->prev = list->tail;
+    node->next = NULL;
+
+    if (NULL != list->tail)
+    {
+        list->tail->next = node;
+    }
+    else
+    {
+        list->head = node;
+    }
+
+    list->tail = node;
+    list->count += 1;
+}
