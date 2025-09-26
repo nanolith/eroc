6a7c9f715b5ea499f6a39b1ad69578b4d8334e3e
Justin Handville <nanolith@gmail.com>
2025-09-26 18:46:27 -0400

    eroc_list_insert_before impl.

---
+++ src/lib/eroc_list_insert_before.c
@@ -0,0 +1,58 @@
+/**
+ * \file lib/eroc_list_insert_before.c
+ *
+ * \brief Insert a node before the given node in a list.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <eroc/list.h>
+
+/**
+ * \brief Insert a node before the given node.
+ *
+ * \param list          The list to use for insertion.
+ * \param before        The node before which this node is inserted.
+ * \param node          The node to insert.
+ */
+void eroc_list_insert_before(
+    eroc_list* list, eroc_list_node* before, eroc_list_node* node)
+{
+    node->next = before;
+
+    /* Inserting before a node. */
+    if (NULL != before)
+    {
+        node->prev = before->prev;
+        if (NULL != before->prev)
+        {
+            /* patch before->prev */
+            before->prev->next = node;
+        }
+        else
+        {
+            /* if before->prev is NULL, then before must be head. */
+            list->head = node;
+        }
+        before->prev = node;
+    }
+    /* Inserting to head. */
+    else
+    {
+        if (NULL != list->head)
+        {
+            node->prev = NULL;
+            list->head->prev = node;
+        }
+        list->head = node;
+    }
+
+    /* fix up tail, if tail is before or NULL. */
+    if (before == list->tail)
+    {
+        list->tail = node;
+    }
+
+    list->count += 1;
+}
