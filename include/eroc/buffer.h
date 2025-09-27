4ed32f5d49ac4f4eef3267598236598fbfad9be7
Justin Handville <nanolith@gmail.com>
2025-09-27 00:29:17 -0400

    eroc_buffer_insert decl.

--- include/eroc/buffer.h
+++ include/eroc/buffer.h
@@ -94,6 +94,20 @@ int eroc_buffer_release(eroc_buffer* buffer);
 int eroc_buffer_append(
     eroc_buffer* buffer, eroc_buffer_line* after, eroc_buffer_line* line);
 
+/**
+ * \brief Insert the given line into the given buffer, before the given line.
+ *
+ * \param buffer            The buffer for this insert operation.
+ * \param before            The line before this line should be inserted, or
+ *                          NULL if this line should be inserted at the
+ *                          beginning of the buffer.
+ * \param line              The line to insert.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_buffer_insert(
+    eroc_buffer* buffer, eroc_buffer_line* before, eroc_buffer_line* line);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
