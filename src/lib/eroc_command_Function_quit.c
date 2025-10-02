256dbd8b5923c014528c279e728f777f2a08e336
Justin Handville <nanolith@gmail.com>
2025-10-02 04:19:08 -0400

    eroc_command_Function_quit impl.

---
+++ src/lib/eroc_command_Function_quit.c
@@ -0,0 +1,29 @@
+/**
+ * \file lib/eroc_command_function_quit.c
+ *
+ * \brief Run the quit command.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <eroc/command.h>
+#include <stdio.h>
+
+/**
+ * \brief Attempt to quit this session.
+ *
+ * \param command           The command instance.
+ * \param lineno            The current line number.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_command_function_quit(eroc_command* command, size_t* lineno)
+{
+    (void)lineno;
+
+    /* set the quit request flag, and let the top level decide what to do. */
+    command->buffer->flags |= EROC_BUFFER_FLAG_QUIT_REQUESTED;
+
+    return 0;
+}
