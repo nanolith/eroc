5041385aefe531ac8fdb4c9bcf7771f102b04c65
Justin Handville <nanolith@gmail.com>
2025-10-02 04:46:25 -0400

    eroc_command_run impl.

---
+++ src/lib/eroc_command_run.c
@@ -0,0 +1,23 @@
+/**
+ * \file lib/eroc_command_run.c
+ *
+ * \brief Run a command.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <eroc/command.h>
+
+/**
+ * \brief Run a command.
+ *
+ * \param command           The command to run.
+ * \param lineno            The current line number.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_command_run(eroc_command* command, size_t* lineno)
+{
+    return command->command_fn(command, lineno);
+}
