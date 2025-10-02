2188200179f8ef87b1156c8ab5e405cb4a4eb16f
Justin Handville <nanolith@gmail.com>
2025-10-02 03:24:01 -0400

    eroc_command_release impl.

---
+++ src/lib/eroc_command_release.c
@@ -0,0 +1,30 @@
+/**
+ * \file lib/eroc_command_release.c
+ *
+ * \brief Release a command.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <eroc/command.h>
+#include <stdlib.h>
+
+/**
+ * \brief Release a command.
+ *
+ * \param command           The command to release.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_command_release(eroc_command* command)
+{
+    if (NULL != command->parameters)
+    {
+        free(command->parameters);
+    }
+
+    free(command);
+
+    return 0;
+}
