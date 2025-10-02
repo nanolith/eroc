f87f9341c4fd9ef6ce9f35003bc765e2c654f607
Justin Handville <nanolith@gmail.com>
2025-10-02 03:54:09 -0400

    eroc_command_parse stub impl.

---
+++ src/lib/eroc_command_parse.c
@@ -0,0 +1,119 @@
+/**
+ * \file lib/eroc_command_parse.c
+ *
+ * \brief Parse a command.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <ctype.h>
+#include <eroc/command.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum parse_token
+{
+    TOK_EOF = -1,
+    TOK_UNKNOWN = 0,
+    TOK_COMMAND_PRINT,
+};
+
+static int command_token_read(const char** input);
+
+/**
+ * \brief Given a command string, a buffer, and the current line, parse the
+ * string into a command.
+ *
+ * \param command           Pointer to the command pointer to set with this
+ *                          command on success.
+ * \param buffer            The buffer on which this command is executed.
+ * \param line              The current line.
+ * \param input             The input string to parse.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_command_parse(
+    eroc_command** command, eroc_buffer* buffer, eroc_buffer_line* line,
+    const char* input)
+{
+    int retval, release_retval, tok;
+    eroc_command* tmp;
+
+    /* create the command to populate. */
+    tmp = (eroc_command*)malloc(sizeof(*tmp));
+    if (NULL == tmp)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* Set initial fields. */
+    memset(tmp, 0, sizeof(*tmp));
+    tmp->buffer = buffer;
+    tmp->line = line;
+
+    do {
+        /* read the first command token. */
+        tok = command_token_read(&input);
+        switch (tok)
+        {
+            case TOK_COMMAND_PRINT:
+                tmp->command_fn = &eroc_command_function_print;
+                goto success;
+
+            case TOK_EOF:
+            case TOK_UNKNOWN:
+                retval = 2;
+                goto done;
+        }
+    } while (TOK_EOF != tok);
+
+success:
+    retval = 0;
+    *command = tmp;
+    tmp = NULL;
+
+done:
+    if (NULL != tmp)
+    {
+        release_retval = eroc_command_release(tmp);
+        if (0 != release_retval)
+        {
+            retval = release_retval;
+        }
+    }
+
+    return retval;
+}
+
+/**
+ * \brief Read the next token from the stream.
+ *
+ * \param input             The input stream, which is updated on read.
+ *
+ * \returns the next token read.
+ */
+static int command_token_read(const char** input)
+{
+    const char* inp = *input;
+
+    /* skip any space before the next token. */
+    while (*inp && isspace(*inp))
+        ++inp;
+
+    switch (*inp)
+    {
+        case 0:
+            *input = inp;
+            return TOK_EOF;
+
+        case 'p':
+            *input = inp + 1;
+            return TOK_COMMAND_PRINT;
+
+        default:
+            *input = inp;
+            return TOK_UNKNOWN;
+    }
+}
