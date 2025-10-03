485bf71a7425d286e8d690b56acd84ef818452be
Justin Handville <nanolith@gmail.com>
2025-10-03 09:45:51 -0400

    eroc_command_function_insert decl.

--- include/eroc/command.h
+++ include/eroc/command.h
@@ -91,6 +91,15 @@ int eroc_command_function_advance(eroc_command* command);
 int eroc_command_function_append(eroc_command* command);
 
 /**
+ * \brief Insert lines terminated by . to the buffer.
+ *
+ * \param command           The command instance.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_command_function_insert(eroc_command* command);
+
+/**
  * \brief Delete lines.
  *
  * \param command           The command instance.
