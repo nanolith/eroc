9d239310baeca018786cd770c3955724d953278a
Justin Handville <nanolith@gmail.com>
2025-10-02 21:32:01 -0400

    eroc_command_function_move impl.

---
+++ src/lib/eroc_command_function_move.c
@@ -0,0 +1,41 @@
+/**
+ * \file lib/eroc_command_function_move.c
+ *
+ * \brief Move the cursor to the given position denoted in start.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <eroc/command.h>
+#include <stdio.h>
+
+/**
+ * \brief Move the cursor to the given position denoted in start.
+ *
+ * \param command           The command instance.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_command_function_move(eroc_command* command)
+{
+    int retval;
+
+    /* this command requires a start address. */
+    if (!command->start_provided)
+    {
+        return 1;
+    }
+
+    /* attempt to move to the provided address. */
+    retval = eroc_buffer_cursor_move(command->buffer, command->start);
+    if (0 != retval)
+    {
+        return retval;
+    }
+
+    /* output the line. */
+    printf("%s\n", command->buffer->cursor->line);
+
+    return 0;
+}
