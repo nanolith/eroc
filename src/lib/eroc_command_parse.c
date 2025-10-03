fc2d19ad9817160eacd7b3ab10112e1a75f8538d
Justin Handville <nanolith@gmail.com>
2025-10-02 22:22:52 -0400

    Add basic address parsing.

--- src/lib/eroc_command_parse.c
+++ src/lib/eroc_command_parse.c
@@ -11,17 +11,37 @@
 #include <eroc/command.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 enum parse_token
 {
     TOK_EOF = -1,
     TOK_UNKNOWN = 0,
+    TOK_COMMAND_MOVE,
     TOK_COMMAND_PRINT,
     TOK_COMMAND_QUIT,
+    TOK_ADDRESS,
 };
 
-static int command_token_read(const char** input);
+typedef struct parse_address parse_address;
+struct parse_address
+{
+    bool sign_set;
+    long value;
+};
+
+typedef union parse_value parse_value;
+union parse_value
+{
+    parse_address address;
+};
+
+static int command_token_read(parse_value* val, const char** input);
 static int command_set(eroc_command* command, int tok);
+static int parse_numeric_address(
+    parse_address* addr, const char* start, const char* end);
+static int translate_relative_address(
+    size_t* abs, const parse_address* rel, const eroc_buffer* buffer);
 
 /**
  * \brief Given a command string and a buffer, parse the string into a command.
@@ -38,6 +58,7 @@ int eroc_command_parse(
 {
     int retval, release_retval, tok;
     eroc_command* tmp;
+    parse_value val;
 
     /* create the command to populate. */
     tmp = (eroc_command*)malloc(sizeof(*tmp));
@@ -54,12 +75,29 @@ int eroc_command_parse(
 
     do {
         /* read the first command token. */
-        tok = command_token_read(&input);
+        tok = command_token_read(&val, &input);
         switch (tok)
         {
             case TOK_COMMAND_PRINT:
             case TOK_COMMAND_QUIT:
                 retval = command_set(tmp, tok);
+                if (0 != retval)
+                {
+                    goto done;
+                }
+                goto success;
+
+            case TOK_ADDRESS:
+                /* TODO - add support for ranged commands. */
+                retval =
+                    translate_relative_address(
+                        &tmp->start, &val.address, buffer);
+                if (0 != retval)
+                {
+                    goto done;
+                }
+                tmp->start_provided = true;
+                retval = command_set(tmp, TOK_COMMAND_MOVE);
                 if (0 != retval)
                 {
                     goto done;
@@ -95,13 +133,16 @@ done:
 /**
  * \brief Read the next token from the stream.
  *
+ * \param val               The parsed value if general.
  * \param input             The input stream, which is updated on read.
  *
  * \returns the next token read.
  */
-static int command_token_read(const char** input)
+static int command_token_read(parse_value* val, const char** input)
 {
+    int retval;
     const char* inp = *input;
+    const char* end;
 
     /* skip any space before the next token. */
     while (*inp && isspace(*inp))
@@ -120,6 +161,19 @@ static int command_token_read(const char** input)
         case 'q':
             *input = inp + 1;
             return TOK_COMMAND_QUIT;
+
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            end = inp + 1;
+            while (*end && isnumber(*end))
+                ++end;
+            retval = parse_numeric_address(&val->address, inp, end);
+            if (0 != retval)
+            {
+                return TOK_UNKNOWN;
+            }
+            *input = end;
+            return TOK_ADDRESS;
 
         default:
             *input = inp;
@@ -147,7 +201,104 @@ static int command_set(eroc_command* command, int tok)
             command->command_fn = &eroc_command_function_quit;
             return 0;
 
+        case TOK_COMMAND_MOVE:
+            command->command_fn = &eroc_command_function_move;
+            return 0;
+
         default:
             return 3;
+    }
+}
+
+/**
+ * \brief Parse the address at [start,end).
+ *
+ * \param addr                  Pointer to the variable to hold the address on
+ *                              success.
+ * \param start                 The start of the address to parse.
+ * \param end                   One past the end of the address.
+ *
+ * \returns 0 on success and non-zero on error.
+ */
+static int parse_numeric_address(
+    parse_address* addr, const char* start, const char* end)
+{
+    const long max_shift_value = LONG_MAX / 10;
+    long value = 0U;
+    bool sign_set = false;
+    bool negative = false;
+
+    /* is the first character a sign? */
+    switch (*start)
+    {
+        case '-':
+            negative = true;
+            /* fall-through. */
+        case '+':
+            sign_set = true;
+            ++start;
+            break;
+    }
+
+    /* loop until the end of input */
+    for (; start != end; ++start)
+    {
+        int digitval = *start - '0';
+
+        /* the value is too big to shift. */
+        if (value > max_shift_value)
+        {
+            return 1;
+        }
+
+        /* shift in the next digit. */
+        value *= 10;
+        value += digitval;
+    }
+
+    /* if the sign is set and the value is 0, set it to 1. */
+    if (sign_set && 0 == value)
+        value = 1;
+
+    /* negate the value if needed. */
+    if (negative) value *= -1;
+
+    /* save the address. */
+    addr->value = value;
+    addr->sign_set = sign_set;
+
+    return 0;
+}
+
+/**
+ * \brief Translate a relative address into an absolute address.
+ *
+ * \param abs               Output parameter for absolute address.
+ * \param rel               The relative address to translate.
+ * \param buffer            The buffer to which this relative address relates.
+ */
+static int translate_relative_address(
+    size_t* abs, const parse_address* rel, const eroc_buffer* buffer)
+{
+    if (rel->sign_set)
+    {
+        long addr = buffer->lineno + rel->value;
+        if (addr < 0 || addr >= (long)buffer->lines->count)
+        {
+            return 1;
+        }
+
+        *abs = addr;
+        return 0;
+    }
+    else
+    {
+        if (rel->value < 1 || rel->value > (long)buffer->lines->count)
+        {
+            return 2;
+        }
+
+        *abs = rel->value - 1;
+        return 0;
     }
 }
