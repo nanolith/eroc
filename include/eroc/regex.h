11950b06b7c21453b45156b8500f44c9f92a1418
Justin Handville <nanolith@gmail.com>
2025-10-04 23:46:27 -0400

    EROC_REGEX_AST_EMPTY decl.

---
+++ include/eroc/regex.h
@@ -0,0 +1,26 @@
+/**
+ * \file eroc/regex.h
+ *
+ * \brief Simple and opinionated DFA regular expression parser, compiler, and
+ * runtime.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#pragma once
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+extern "C" {
+# endif /*__cplusplus*/
+
+enum eroc_regex_ast_node_type
+{
+    EROC_REGEX_AST_EMPTY,
+};
+
+/* C++ compatibility. */
+# ifdef   __cplusplus
+}
+# endif /*__cplusplus*/
