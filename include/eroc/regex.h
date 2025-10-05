54186164ba3e0784b39767866d614ca272100a5e
Justin Handville <nanolith@gmail.com>
2025-10-05 00:34:07 -0400

    eroc_regex_ast_node_alternate_create decl.

--- include/eroc/regex.h
+++ include/eroc/regex.h
@@ -106,6 +106,23 @@ int eroc_regex_ast_node_concat_create(
     eroc_regex_ast_node** node, eroc_regex_ast_node* left,
     eroc_regex_ast_node* right);
 
+/**
+ * \brief Create an alternate AST node.
+ *
+ * \note On success, the created node takes ownership of the left and right
+ * nodes.
+ *
+ * \param node          Pointer to the AST node pointer to set to the created
+ *                      node on success.
+ * \param left          The left-hand side of the alternate.
+ * \param right         The right-hand side of the alternate.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_regex_ast_node_alternate_create(
+    eroc_regex_ast_node** node, eroc_regex_ast_node* left,
+    eroc_regex_ast_node* right);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
