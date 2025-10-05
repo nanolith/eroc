b58cc1ac59e80f1d85b135e40259f64836e4a37a
Justin Handville <nanolith@gmail.com>
2025-10-05 00:38:19 -0400

    eroc_regex_ast_node_plus_create decl.

--- include/eroc/regex.h
+++ include/eroc/regex.h
@@ -149,6 +149,20 @@ int eroc_regex_ast_node_char_class_create(
 int eroc_regex_ast_node_star_create(
     eroc_regex_ast_node** node, eroc_regex_ast_node* star);
 
+/**
+ * \brief Create a plus AST node.
+ *
+ * \note On success, the created node takes ownership of the child node.
+ *
+ * \param node          Pointer to the AST node pointer to set to the created
+ *                      node on success.
+ * \param plus          The child of this plus node.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_regex_ast_node_plus_create(
+    eroc_regex_ast_node** node, eroc_regex_ast_node* plus);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
