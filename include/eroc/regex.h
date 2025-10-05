fd869f9e59f3ecd64f6d6f673cc577dec9762209
Justin Handville <nanolith@gmail.com>
2025-10-05 00:35:55 -0400

    eroc_regex_ast_node_char_class_create decl.

--- include/eroc/regex.h
+++ include/eroc/regex.h
@@ -123,6 +123,18 @@ int eroc_regex_ast_node_alternate_create(
     eroc_regex_ast_node** node, eroc_regex_ast_node* left,
     eroc_regex_ast_node* right);
 
+/**
+ * \brief Create a character class AST node.
+ *
+ * \param node          Pointer to the AST node pointer to set to the created
+ *                      node on success.
+ * \param members       A bitset of the members of this character class.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_regex_ast_node_char_class_create(
+    eroc_regex_ast_node** node, uint32_t members[8]);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
