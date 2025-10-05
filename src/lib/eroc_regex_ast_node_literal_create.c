346232fd5ed8376511a8f823e5996c7245dd84ba
Justin Handville <nanolith@gmail.com>
2025-10-05 01:11:18 -0400

    eroc_regex_ast_node_literal_create impl.

---
+++ src/lib/eroc_regex_ast_node_literal_create.c
@@ -0,0 +1,35 @@
+/**
+ * \file lib/eroc_regex_ast_node_literal_create.c
+ *
+ * \brief Create a char literal AST node.
+ *
+ * \copyright 2025 Justin Handville.  Please see LICENSE.txt in this
+ * distribution for the license terms under which this software is distributed.
+ */
+
+#include <eroc/regex.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * \brief Create a char literal AST node.
+ *
+ * \param node          Pointer to the AST node pointer to set to the created
+ *                      node on success.
+ * \param c             The character literal.
+ *
+ * \returns 0 on success and non-zero on failure.
+ */
+int eroc_regex_ast_node_literal_create(eroc_regex_ast_node** node, char c)
+{
+    int retval = eroc_regex_ast_node_empty_create(node);
+    if (0 != retval)
+    {
+        return retval;
+    }
+
+    (*node)->type = EROC_REGEX_AST_LITERAL;
+    (*node)->data.literal = c;
+
+    return 0;
+}
