66b5f470d26ad90085c431d941b044bf9d25df74
Justin Handville <nanolith@gmail.com>
2025-10-10 04:48:10 -0400

    Test that we can escape keywords to get literals.

--- test/lib/test_eroc_regex_compiler_parse.cpp
+++ test/lib/test_eroc_regex_compiler_parse.cpp
@@ -614,3 +614,26 @@ TEST(parse_single_literal)
     /* clean up. */
     eroc_regex_ast_node_release(ast);
 }
+
+/**
+ * \brief We can parse an escaped literal.
+ */
+TEST(parse_escaped_literal)
+{
+    eroc_regex_ast_node* ast;
+    const char* INPUT = R"(\[)";
+
+    TEST_ASSERT(0 == eroc_regex_compiler_parse(&ast, INPUT));
+
+    /* ast is not NULL. */
+    TEST_ASSERT(nullptr != ast);
+    /* ast does not have a next pointer. */
+    TEST_EXPECT(nullptr == ast->next);
+    /* ast's type is ANY. */
+    TEST_ASSERT(EROC_REGEX_AST_LITERAL == ast->type);
+    /* the literal is '['. */
+    TEST_EXPECT('[' == ast->data.literal);
+
+    /* clean up. */
+    eroc_regex_ast_node_release(ast);
+}
