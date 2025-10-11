8d9c60e4bc0a69158d46501ee597a515f5b1417e
Justin Handville <nanolith@gmail.com>
2025-10-11 18:48:26 -0400

    eroc_avl_tree_rotate_left decl.

--- include/eroc/avltree.h
+++ include/eroc/avltree.h
@@ -229,6 +229,16 @@ eroc_avl_tree_node* eroc_avl_tree_predecessor_node(
  */
 void eroc_avl_tree_rotate_right(eroc_avl_tree_node** root);
 
+/**
+ * \brief Perform a left rotation on an AVL tree.
+ *
+ * Root will become root->right's left-most child, and root->right will become
+ * the new root of this subtree.
+ *
+ * \param root          The root for this rotation.
+ */
+void eroc_avl_tree_rotate_left(eroc_avl_tree_node** root);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
