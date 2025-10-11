61904d6ca4aa7ec08b6f35d6b153e010c72ad02b
Justin Handville <nanolith@gmail.com>
2025-10-11 06:20:26 -0400

    eroc_avl_tree_minimum_node decl.

--- include/eroc/avltree.h
+++ include/eroc/avltree.h
@@ -173,6 +173,18 @@ bool eroc_avl_tree_delete(
  */
 void eroc_avl_tree_swap(eroc_avl_tree* left, eroc_avl_tree* right);
 
+/**
+ * \brief Given a subtree node, return the minimum node in this subtree.
+ *
+ * \param tree          The AVL tree for this operation.
+ * \param x             The node from which this search should start. Must not
+ *                      be NULL.
+ *
+ * \returns the minimum node in this subtree.
+ */
+eroc_avl_tree_node* eroc_avl_tree_minimum_node(
+    eroc_avl_tree* tree, eroc_avl_tree_node* x);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
